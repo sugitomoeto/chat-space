223e1158f988d177e40b53df5e3f11f47cb1ee0c
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-06-20 20:32:10 +0900

    Add sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current.user.update(user_params)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
