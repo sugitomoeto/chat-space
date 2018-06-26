f8f2bc038a65cd8e6ff40a9bbe1d90ad32a6a25f
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-06-26 13:20:26 +0900

    Messages アクションの具体的な記述(index,create)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
-  def index
-    # チャットの表示
+  def index # チャットの表示
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
-  def create
-    # チャットの保存
+  def create # チャットの保存
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group)
+    else
+      @messages = @group.messages.includes(:user)
+      render :index
+    end
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
