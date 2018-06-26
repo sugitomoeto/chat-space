229ee49346e9e7397379bb9c3d2864bd19c59cbb
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-06-26 16:14:18 +0900

    Change view(最新メッセージ表示)

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,12 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  def show_last_message
+    if (last_massage = messages.last).present?
+      last_massage.content? ? last_massage.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。。'
+    end
+  end
 end
