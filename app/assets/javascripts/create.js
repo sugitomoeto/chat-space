4a3dc7a35c3df54574956faa0f409f9613a88084
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-07-08 19:20:54 +0900

    Add HTML(HTML表示・自動スクロール)

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,8 +1,31 @@
 $(function(){
+
+  function buildHTML(message){
+    var img = message.image.url ? `<img src= ${message.image.url}>` : "";
+    var html = `<div class="main__middle__messages">
+                  <div class="main__middle__messages__user-name">
+                    ${ message.name }
+                  </div>
+                  <div class="main__middle__messages__posted-time">
+                    ${ message.created_at}
+                  </div>
+                  <div class="main__middle__messages__content">
+                    ${ message.content}
+                  </div>
+                  <div class="main__middle__messages__content">
+                    ${ img }
+                  </div>
+                </div>`
+    return html;
+  }
+
+
+
   $('#new_message').on("submit", function(e){
     e.preventDefault();
     console.log("waa");
-    var formData = new FormData(this);
+    var $this = $(this);
+    var formData = new FormData($this.get(0));
     var url = $(this).attr('action');
     $.ajax({
       url: url,
@@ -12,5 +35,13 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      var html = buildHTML(message);
+      var middle = $(".main__middle")
+      middle.append(html);
+      middle.animate({scrollTop: middle[0].scrollHeight},'fast');
+      $this.get(0).reset();
+    })
+    return false;
   });
 });
