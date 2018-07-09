d97f4d53b8a8c346bab181d70ae0a5b2af8f0d61
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-07-09 19:31:12 +0900

    Delete comment-out(不要なコメントアウトを削除)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,13 @@
 class MessagesController < ApplicationController
   before_action :set_group
 
-  def index # チャットの表示
+  def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
 
-  def create # チャットの保存
+  def create
     @message = @group.messages.new(message_params)
-    # if @message.save
-    #   redirect_to group_messages_path(@group), notice: 'メッセージが表示されました。'
-    # else
-    #   @messages = @group.messages.includes(:user)
-    #   flash.now[:alert] = 'メッセージを入力してください。'
-    #   render :index
-    # end
     if @message.save
       respond_to do |format|
         format.html{ redirect_to group_messages_path(@group) }
