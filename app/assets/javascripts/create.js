9a46e17919cd4c5dd593b3eaa564ee17d03aee12
sugitomoeto <moeto@sugitomoetonoMacBook-puro.local>
2018-07-24 15:06:01 +0900

    Add setInterval(自動更新機能追加)

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,16 +1,16 @@
 $(function(){
 
   function buildHTML(message){
-    var img = message.image ? `<img src= ${message.image}>` : "";
-    var html = `<div class="main__middle__messages">
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="main__middle__messages" data-massege="${ message.id }">
                   <div class="main__middle__messages__user-name">
                     ${ message.name }
                   </div>
                   <div class="main__middle__messages__posted-time">
-                    ${ message.created_at}
+                    ${ message.created_at }
                   </div>
                   <div class="main__middle__messages__content">
-                    ${ message.content}
+                    ${ message.content }
                   </div>
                   <div class="main__middle__messages__content">
                     ${ img }
@@ -46,4 +46,33 @@ $(function(){
     })
     return false;
   });
+
+  var interval = setInterval(function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+        $.ajax({
+          url: location.href,
+          type: "GET",
+          dataType: 'json',
+          processData: false,
+          contentType: false,
+        })
+        .done(function(json){
+          var id = $('.main__middle__messages').last().data('message')
+          var insertHTML = '';
+          json.messages.forEach(function(message){
+            if (message.id > id){
+              insertHTML = buildHTML(message)
+            }
+          })
+          $('.main__middle').append(insertHTML)
+          scrollDown($('.main__middle'))
+        })
+        .fail(function(){
+          alert('メッセージが取得出来ませんでした。');
+        })
+      }
+    else {
+      clearInterval(interval)
+    }
+  }, 5000 );
 });
